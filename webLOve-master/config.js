c01766333bca4003a990e565d23e1382d8b93b97
Hoan-nguyendo <111550702+Hoan-nguyendo@users.noreply.github.com>
2023-09-24 14:56:08 +0700

    Add files via upload

---
+++ webLOve-master/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello emmm yêu dấu của anhhhhh!",
+  introDesc: `Trái đất vốn lạ thường\n
+    Mà sao anh cứ đi nhầm đường\n
+    Lạc vào tim em lẻ loi\n
+    Đằng sau chữ yêu đây là thương`,
+  btnIntro: "hihi",
+  title: "Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng anh đã mê ngay từ lúc thấy nụ cười ấy ",
+  btnNo: "Hứ tui khum yêu anhhh <33",
+  btnYes: "Em yêu anh rấc nhiều ạaa >.<",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi cho anhhh <3",
+  reply: "Bởi vì anh yêu em, yêu emm rấc nhiều và tui bỏ bùa anh rùiiii::!<33333333",
+  mess: "Anh biết mà 🥰. Anh xứng đáng có được emmmmm! 😘😘",
+  messDesc: "Anh sẽ bên em đến tận cùng thời gian nhé babiii :3",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://m.me/NguyenDoHoan812", 
+};
